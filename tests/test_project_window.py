6a438a1a432bcf9d3025b3f7a1e794cc84bb758e
T. C <37833294+mcbaguetti@users.noreply.github.com>
2023-04-20 15:30:22 +0200

    Update test_project_window.py
    added more tests

--- tests/test_project_window.py
+++ tests/test_project_window.py
@@ -1,60 +1,47 @@
 # import time
-from pytest import raises
+import pytest
+from pzero.entities_factory import DEM
 
 from pzero.project_window import ProjectWindow
 
-from PyQt5.QtGui import QCloseEvent, QShowEvent
-from PyQt5.QtCore import Qt, QEvent
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QMainWindow
+from PyQt5.QtWidgets import QWidget
 
-
-# Click yes on the dialog box
-def handle_dialog_yes(qtbot, window):
-    # Get the message box for saving the project and the no_button
-    messagebox = window.findChild(QMessageBox)
-    # messagebox = QApplication.activeWindow()
-    print(messagebox)
-    yes_button = messagebox.button(QMessageBox.Yes)
-
-    # Close PZero
-    qtbot.mouseClick(yes_button, Qt.LeftButton)
-
-
-# Click no on the dialog box
-def handle_dialog_no(qtbot, window):
-    # Get the message box for closing the project and the yes_button
-    messagebox = window.findChild(QMessageBox)
-    # messagebox = QApplication.activeWindow()
-    print(messagebox)
-    no_button = messagebox.button(QMessageBox.No)
-
-    # Don't save the project
-    qtbot.mouseClick(no_button, Qt.LeftButton)
+# Global Var to set if the test are automatic or not
+automatic_test = True
 
 
 # Class for testing the project window, qtbot part of a plugin of pytestQt
 class TestProjectWindow:
+    test_vtk_obj = DEM()
+
+    geological_entity_dict = {'uid': "0",
+                              'name': "geoname",
+                              'topological_type': "topol",
+                              'geological_type': "undef",
+                              'geological_feature': "undef",
+                              'scenario': "sc1",
+                              'properties_names': [],
+                              'properties_components': [],
+                              'x_section': "",
+                              'vtk_obj': test_vtk_obj}
 
     def ignore(self):
         return
 
     def test_is_window(self, qtbot):
         project_window = ProjectWindow()
-        project_window.show()
 
         print(project_window.isWindow())
 
-        assert project_window.isWindow() == True
+        assert project_window.isWindow() is True
 
     def test_window_name(self, qtbot):
         project_window = ProjectWindow()
-        project_window.show()
 
         assert project_window.windowTitle() == "PZero"
 
     def test_shown_table(self, qtbot):
         project_window = ProjectWindow()
-        project_window.show()
 
         shown_table = project_window.shown_table
         # print(shown_table)
@@ -63,68 +50,75 @@ class TestProjectWindow:
 
     def test_shown_table_change(self, qtbot):
         project_window = ProjectWindow()
-        project_window.show()
-
-        tabImg = 'tabImages'
-        # tabGeol = 'tabGeology'
-
-        pageImg = project_window.tabCentral.findChild(QWidget, tabImg)
-        # pageGeol = project_window.tabCentral.findChild(QWidget, tabGeol)
-
-        project_window.tabCentral.setCurrentWidget(pageImg)
-        shown_table = project_window.shown_table
-        # print(shown_table)
-
-        assert shown_table == tabImg
-
-    def test_shown_table_change(self, qtbot):
-        project_window = ProjectWindow()
-        project_window.show()
-
-        tabImg = 'tabImages'
-        # tabGeol = 'tabGeology'
-
-        pageImg = project_window.tabCentral.findChild(QWidget, tabImg)
-        # pageGeol = project_window.tabCentral.findChild(QWidget, tabGeol)
-
-        project_window.tabCentral.setCurrentWidget(pageImg)
-        shown_table = project_window.shown_table
-        # print(shown_table)
-
-        assert shown_table == tabImg
-
-    def test_shown_table_change(self, qtbot):
-        project_window = ProjectWindow()
-        project_window.show()
 
         tab_img = 'tabImages'
-        # tabGeol = 'tabGeology'
 
         page_img = project_window.tabCentral.findChild(QWidget, tab_img)
-        # pageGeol = project_window.tabCentral.findChild(QWidget, tabGeol)
 
         project_window.tabCentral.setCurrentWidget(page_img)
         shown_table = project_window.shown_table
-        # print(shown_table)
 
         assert shown_table == tab_img
 
-
-
-        """
-    # TO FIX
-    # Testing the close event
-    def test_close_event(self, qtbot):
-
-        # setting the projectWindow
+    def test_none_selected_uids(self, qtbot):
         project_window = ProjectWindow()
-        project_window.show()
 
-        # call the close event
-        project_window.closeEvent(event=self)
+        assert project_window.selected_uids == []
 
-        handle_dialog_no(qtbot, project_window)
-        handle_dialog_yes(qtbot, project_window)
+    def test_selected_uids(self, qtbot):
+        project_window = ProjectWindow()
 
-        #assert project_window.closeEvent == QEvent.accept()
-    """
+        # add an entity and then select all the entities on the geology View
+        project_window.geol_coll.add_entity_from_dict(self.geological_entity_dict)
+        project_window.GeologyTableView.selectAll()
+
+        # check that the length of the selected uids is equal to the added entity inside the geol coll
+        assert len(project_window.selected_uids) == 1
+
+    def test_entity_remove(self, qtbot, monkeypatch):
+        # monkeypatch/mock function
+        def mock_entity_remove():
+            self.update_actors = False
+            for uid in project_window.selected_uids:
+                if project_window.shown_table == "tabGeology":
+                    project_window.geol_coll.remove_entity(uid=uid)
+            self.update_actors = True
+
+        project_window = ProjectWindow()
+
+        # add an entity and then select all the entities on the geology View
+        project_window.geol_coll.add_entity_from_dict(self.geological_entity_dict)
+        project_window.GeologyTableView.selectAll()
+
+        # remove the entity through the mock function 'entity_remove'
+        mock_entity_remove()
+
+        assert len(project_window.selected_uids) == 0
+
+    # Testing the add property event - running only manually, to try change the var automatic_test to false
+    # or comment the next @pytest line
+    @pytest.mark.skipif(automatic_test, reason="Button-clicks not implemented yet")
+    def test_property_add(self, qtbot):
+        project_window = ProjectWindow()
+
+        project_window.geol_coll.add_entity_from_dict(self.geological_entity_dict)
+        project_window.GeologyTableView.selectAll()
+
+        project_window.property_add()
+
+        assert project_window.geol_coll.get_uid_properties_names(uid="0") == ["new_property"]
+
+    # Testing the add and remove property event - running only manually, to try change the var automatic_test to false
+    # or comment @pytest line
+    @pytest.mark.skipif(automatic_test, reason="Button-clicks not implemented yet")
+    def test_property_remove(self, qtbot):
+        project_window = ProjectWindow()
+
+        project_window.geol_coll.add_entity_from_dict(self.geological_entity_dict)
+        project_window.GeologyTableView.selectAll()
+
+        project_window.property_add()
+        project_window.property_remove()
+
+        assert project_window.geol_coll.get_uid_properties_names(uid="0") == []
+
