8b96f7a2a123e74a66530b8b698e138e09c5ef97
T. C <37833294+mcbaguetti@users.noreply.github.com>
2023-04-24 10:18:55 +0200

    create test_helper_dialogs

---
+++ tests/test_helpers/test_helper_dialogs.py
@@ -0,0 +1,93 @@
+from PyQt5.QtWidgets import QMessageBox, QLabel
+
+from pzero.helpers.helper_dialogs import options_dialog, progress_dialog
+
+"""
+def test_option_dialogs(self, qtbot):
+    title = "test_dialog"
+    message = "this is a test message"
+    res = options_dialog(title=title, message=message, yes_role="yes", no_role="no")
+
+    qtbot.mouseClick()
+
+    # if pressed no res == 1, otherwise res == 0
+    assert res == 1
+"""
+
+
+# Testing the class progress_dialog()
+class TestProgressDialog:
+
+    # testing if the initial dialog values are correct
+    def test_init_dialog(self, qtbot):
+        max_value = 1
+        title = "Title_test"
+        label = "Saving test"
+
+        progress_dialog_instance = progress_dialog(max_value=max_value, title_txt=title,
+                                                   label_txt=label, cancel_txt=None, parent=self)
+
+        assert progress_dialog_instance.value() == -1 \
+            and progress_dialog_instance.maximum() == max_value \
+            and progress_dialog_instance.windowTitle() == title \
+            and progress_dialog_instance.labelText() == label
+
+    # Testing the add_one function
+    def test_add_one(self, qtbot):
+        max_value = 5000
+        title = "Title_test"
+        label = "Saving test"
+
+        progress_dialog_instance = progress_dialog(max_value=max_value, title_txt=title,
+                                                   label_txt=label, cancel_txt=None, parent=self)
+
+        for i in range(max_value):
+            progress_dialog_instance.add_one()
+
+        # we use max_value -1 because the value in the dialog starts with -1
+        assert progress_dialog_instance.value() == max_value - 1
+
+    # Testing was_canceled in the progress dialog
+    def test_was_canceled(self, qtbot):
+        max_value = 1
+        title = "Title_test"
+        label = "Saving test"
+        cancel_button_text = "test delete me"
+
+        progress_dialog_instance = progress_dialog(max_value=max_value, title_txt=title,
+                                                   label_txt=label, cancel_txt=cancel_button_text, parent=self)
+
+        assert progress_dialog_instance.wasCanceled() is False
+
+    # Testing with calling the cancel button in the progress dialog
+    def test_was_canceled_true(self, qtbot):
+        max_value = 1
+        title = "Title_test"
+        label = "Saving test"
+        cancel_button_text = "test delete me"
+
+        progress_dialog_instance = progress_dialog(max_value=max_value, title_txt=title,
+                                                   label_txt=label, cancel_txt=cancel_button_text, parent=self)
+        progress_dialog_instance.cancel()
+
+        assert progress_dialog_instance.wasCanceled() is True
+
+    # Testing change in the dialog
+    def test_change_dialog_label(self, qtbot):
+        max_value = 5000
+        title = "Title_test"
+        label = "Saving test"
+        change_label = "new_test_label"
+
+        progress_dialog_instance = progress_dialog(max_value=max_value, title_txt=title,
+                                                   label_txt=label, cancel_txt=None, parent=self)
+        progress_dialog_instance.setLabelText(change_label)
+
+        assert progress_dialog_instance.labelText() == change_label
+
+
+"""
+class TestPCDataModel:
+
+    def
+"""
