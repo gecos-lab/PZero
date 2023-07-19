ebd16d8053ee99c97e10b03911ebbc43a5b01a0a
andrea-bistacchi <andrea.bistacchi@unimib.it>
2023-07-19 11:30:57 +0200

    added option to impor color from Gocad ASCII

--- pzero/imports/obj2vtk.py
+++ pzero/imports/obj2vtk.py
@@ -4,42 +4,70 @@ PZero© Andrea Bistacchi"""
 from vtk import vtkOBJWriter
 
 from pzero.entities_factory import TriSurf
+from pzero.helpers.helper_dialogs import options_dialog
 
 
 def vtk2obj(self=None, out_dir_name=None):
     """Exports all triangulated surfaces to a collection of OBJ surfaces.
     IN THE FUTURE extendo to other entity classes such as DEM, polyline, etc."""
+    append_name = options_dialog(
+        title="Append name",
+        message="Append entity name to output file mane?",
+        yes_role="Yes",
+        no_role="No",
+        reject_role=None,
+    )
+    if append_name == 0:
+        append_name = True
+    else:
+        append_name = False
     """Create STL writer."""
     obj_writer = vtkOBJWriter()
     """Loop for each entity."""
     for uid in self.geol_coll.df["uid"]:
         if isinstance(self.geol_coll.get_uid_vtk_obj(uid), TriSurf):
-            out_file_name = (
-                str(out_dir_name)
-                + "/"
-                + uid
-                + "_"
-                + self.geol_coll.df.loc[self.geol_coll.df["uid"] == uid, "name"].values[
-                    0
-                ]
-                + ".obj"
-            )
+            if append_name:
+                out_file_name = (
+                    str(out_dir_name)
+                    + "/"
+                    + uid
+                    + "_"
+                    + self.geol_coll.df.loc[self.geol_coll.df["uid"] == uid, "name"].values[
+                        0
+                    ]
+                    + ".obj"
+                )
+            else:
+                out_file_name = (
+                        str(out_dir_name)
+                        + "/"
+                        + uid
+                        + ".obj"
+                )
             vtk_entity = self.geol_coll.get_uid_vtk_obj(uid)
             obj_writer.SetFileName(out_file_name)
             obj_writer.SetInputData(vtk_entity)
             obj_writer.Write()
     for uid in self.boundary_coll.df["uid"]:
         if isinstance(self.boundary_coll.get_uid_vtk_obj(uid), TriSurf):
-            out_file_name = (
-                str(out_dir_name)
-                + "/"
-                + uid
-                + "_"
-                + self.boundary_coll.df.loc[
-                    self.boundary_coll.df["uid"] == uid, "name"
-                ].values[0]
-                + ".obj"
-            )
+            if append_name:
+                out_file_name = (
+                    str(out_dir_name)
+                    + "/"
+                    + uid
+                    + "_"
+                    + self.boundary_coll.df.loc[
+                        self.boundary_coll.df["uid"] == uid, "name"
+                    ].values[0]
+                    + ".obj"
+                )
+            else:
+                out_file_name = (
+                        str(out_dir_name)
+                        + "/"
+                        + uid
+                        + ".obj"
+                )
             vtk_entity = self.boundary_coll.get_uid_vtk_obj(uid)
             obj_writer.SetFileName(out_file_name)
             obj_writer.SetInputData(vtk_entity)
