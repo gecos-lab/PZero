fd7dfaeb1320f4e0baebcf5e21eb27580d0e92ee
waqashussain117 <150252664+waqashussain117@users.noreply.github.com>
2023-11-13 10:18:18 +0100

    Update: Simplified File Identification and Cleaned Up Code in segy2vtk
    Key Updates:
    1. Changed how files are identified: Instead of using a complex unique identifier (UUID), now the file's own name is used for easier tracking. This makes it consistent with how other parts of the software identify files.
    
    2. Made the code cleaner and simpler: Removed some unnecessary steps in the code, making it easier to read and understand. This helps with future updates and maintenance.
    
    3. Streamlined how file attributes are handled: Adjusted the way the software keeps track of file information, removing unneeded details and aligning it better with the rest of the software.
    
    4. Better error messages: Updated the way the software reports errors during file import, making it easier to understand what went wrong.
    
    These updates aim to make the software more user-friendly and maintainable by simplifying some of its core functions.

--- pzero/imports/segy2vtk.py
+++ pzero/imports/segy2vtk.py
@@ -2,43 +2,39 @@
 PZero© Andrea Bistacchi"""
 
 import os
-import uuid
 from copy import deepcopy
 
 from vtk import vtkSegYReader
-
 from pzero.collections.image_collection import ImageCollection
 from pzero.entities_factory import Seismics
-
-"""Very basic SEG-Y importer. TO BE IMPROVED IN THE FUTURE."""
-
 
 def segy2vtk(self=None, in_file_name=None):
     """Import and add a SEGY seismics cube to the mesh3d_coll of the project.
     <self> is the calling ProjectWindow() instance."""
     try:
-        """Do not use the StructuredGridOff() option, this causes problems.
-        https://vtk.org/doc/nightly/html/classvtkSegYReader.html#aa1e0a8e126958a91b3106159a2680041
-        """
-        curr_object = Seismics()  # ________________________________________
+        curr_object = Seismics()  # Create a Seismics object
         segy_reader = vtkSegYReader()
         segy_reader.SetFileName(in_file_name)
         segy_reader.Update()
         curr_object.ShallowCopy(segy_reader.GetOutput())
         curr_object.Modified()
-        """Create dictionary."""
-        curr_obj_attributes = deepcopy(ImageCollection.image_entity_type_dict)
-        curr_obj_attributes["uid"] = str(uuid.uuid4())
-        curr_obj_attributes["name"] = os.path.basename(in_file_name)
-        curr_obj_attributes["image_type"] = "Seismics"
-        curr_obj_attributes["properties_names"] = curr_object.point_data_keys
-        # curr_obj_attributes['properties_components'] = curr_object.point_data_components
-        curr_obj_attributes["properties_types"] = curr_object.properties_types
-        curr_obj_attributes["vtk_obj"] = curr_object
-        """Add to entity collection."""
+
+        # Create dictionary with uid instead of uuid
+        curr_obj_attributes = {
+            "uid": os.path.basename(in_file_name),  # Using file name as uid for simplicity
+            "name": os.path.basename(in_file_name),
+            "image_type": "Seismics",  # Updated to align with other parts of your code
+            "properties_names": curr_object.point_data_keys,
+            "properties_components": curr_object.point_data_components,
+            "vtk_obj": curr_object,
+            # Other attributes can be added as needed
+        }
+
+        # Add to image collection
         self.image_coll.add_entity_from_dict(entity_dict=curr_obj_attributes)
-        """Cleaning."""
+
+        # Cleaning up
         del curr_object
         del curr_obj_attributes
-    except:
-        self.TextTerminal.appendPlainText("SEGY file not recognized ERROR.")
+    except Exception as e:
+        self.TextTerminal.appendPlainText(f"SEGY file not recognized ERROR: {str(e)}")
