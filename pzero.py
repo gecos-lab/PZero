2f3d99092f68ab31b5ed40a5bb8c97b7c4682f2a
andrea-bistacchi <andrea.bistacchi@unimib.it>
2024-09-30 09:30:02 +0200

    fixed high- vs. low-dpi scaling

--- pzero.py
+++ pzero.py
@@ -5,6 +5,15 @@ from sys import argv, exit
 
 from PyQt5.QtWidgets import QApplication
 
+from PyQt5.QtCore import Qt
+
+if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+
+
 from pzero.project_window import ProjectWindow
 
 app = QApplication(argv)
