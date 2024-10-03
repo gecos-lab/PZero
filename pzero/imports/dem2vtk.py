cd27af5eabd7d359250fccaeeecb01f643f0b55f
waqashussain117 <waqas.hussain@unimib.it>
2024-10-03 16:07:41 +0200

    Error in importing DEM files to PZERO:
    The error message indicates that xarray does not recognize 'rasterio' as a valid engine. This usually happens when the 'rasterio' backend is not registered with xarray.
    In recent versions of xarray (0.18 and above), the way to read raster files (like GeoTIFFs) has changed.
    Solution in the new version is to import rioxarray.
    
    To fix this issue, we should use rioxarray.open_rasterio() instead of xr.open_dataset(). The function rioxarray.open_rasterio() returns an xarray.DataArray represents a single raster band and has a .values attribute that we can use to access the raster data directly.
    
    two lines of code changed:
    
    data = xr.open_dataset(in_file_name, engine='rasterio')
    to
    data = rioxarray.open_rasterio(in_file_name)
    
    and in order to access the raster data. We should use data.values instead of data.data
    so changing
    
    values = data.values[0] # Assuming single-band raster

--- pzero/imports/dem2vtk.py
+++ pzero/imports/dem2vtk.py
@@ -2,68 +2,37 @@
 PZero© Andrea Bistacchi"""
 
 import os
-
 from uuid import uuid4
-
 from copy import deepcopy
 
-from xarray import open_dataset as xr_open_dataset
-
+import xarray as xr
+import rioxarray  # Import rioxarray to register the 'rasterio' engine
 from numpy import any as np_any
 from numpy import asarray as np_asarray
 from numpy import meshgrid as np_meshgrid
 from numpy import nan as np_nan
-
 from pyvista import StructuredGrid as pv_StructuredGrid
 
 from pzero.collections.dom_collection import DomCollection
 from pzero.collections.fluid_collection import FluidCollection
 from pzero.entities_factory import DEM
 
-# from AbstractImporter import BaseIO
-#
-#
-# class DomIO(BaseIO):
-#
-#     def __init__(self, input_file):
-#         super().__init__(input_file)
-#
-#     def import_from_file(self):
-#         """Read raster file format (geotiff) with xarray and rasterio and create DEM structured grid.
-#             Helpful: http://xarray.pydata.org/en/stable/auto_gallery/plot_rasterio.html
-#             https://github.com/pyvista/pyvista-support/issues/205, thanks to Bane Sullivan"""
-#         data = xr_open_rasterio(self.input_file)
-#         values = np_asarray(data)
-#         nans = values == data.nodatavals
-#         if np_any(nans):
-#             values[nans] = np_nan
-#         xx, yy = np_meshgrid(data["x"], data["y"])
-#         zz = values.reshape(xx.shape)
-#         """Convert to DEM() instance."""
-#         curr_obj = DEM()
-#         temp_obj = pv_StructuredGrid(xx, yy, zz)
-#         temp_obj["elevation"] = zz.ravel(order="F")
-#         curr_obj.ShallowCopy(temp_obj)
-#         curr_obj.Modified()
-#
-#     @staticmethod
-#     def output_to_file():
-#         pass
-
-
 def dem2vtk(self=None, in_file_name=None, collection=None):
     """Import and add a DEM structured grid to the dom_coll of the project.
     <self> is the calling ProjectWindow() instance."""
-    """Read raster file format (geotiff) with xarray and rasterio and create DEM structured grid.
-    Helpful: http://xarray.pydata.org/en/stable/auto_gallery/plot_rasterio.html
-    https://github.com/pyvista/pyvista-support/issues/205, thanks to Bane Sullivan"""
-    data = xr_open_dataset(in_file_name, engine='rasterio')
-    values = np_asarray(data)
-    nans = values == data.nodatavals
+    # Read raster file format (e.g., GeoTIFF) with xarray and rasterio
+    data = rioxarray.open_rasterio(in_file_name)
+    # Alternatively, you can use:
+    # data = rioxarray.open_rasterio(in_file_name)
+
+    # Access the raster data
+    values = data.values[0]  # Assuming single-band raster
+    nans = values == data.rio.nodata
     if np_any(nans):
+        values = values.astype(float)
         values[nans] = np_nan
-    xx, yy = np_meshgrid(data["x"], data["y"])
-    zz = values.reshape(xx.shape)
+    xx, yy = np_meshgrid(data['x'], data['y'])
+    zz = values
     """Convert to DEM() instance."""
     curr_obj = DEM()
     temp_obj = pv_StructuredGrid(xx, yy, zz)
@@ -72,7 +41,7 @@ def dem2vtk(self=None, in_file_name=None, collection=None):
     curr_obj.Modified()
     """Create dictionary."""
     if collection == "DEMs and DOMs":
-        curr_obj_attributes = deepcopy(DomCollection.entity_dict)
+        curr_obj_attributes = deepcopy(self.dom_coll.entity_dict)
         curr_obj_attributes["uid"] = str(uuid4())
         curr_obj_attributes["name"] = os.path.basename(in_file_name)
         curr_obj_attributes["topology"] = "DEM"
@@ -83,7 +52,7 @@ def dem2vtk(self=None, in_file_name=None, collection=None):
         """Add to entity collection."""
         self.dom_coll.add_entity_from_dict(entity_dict=curr_obj_attributes)
     elif collection == "Fluid contacts":
-        curr_obj_attributes = deepcopy(FluidCollection.entity_dict)
+        curr_obj_attributes = deepcopy(self.fluid_coll.entity_dict)
         curr_obj_attributes["uid"] = str(uuid4())
         curr_obj_attributes["name"] = os.path.basename(in_file_name)
         curr_obj_attributes["role"] = "raster"
