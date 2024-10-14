c1026ab3ca5b932e4f5d6c4eae97bab48fae021e
waqashussain117 <waqas.hussain@unimib.it>
2024-10-14 14:24:34 +0200

    Taking clues from dem2vtk to correct pyvista2vtk.py. Error: entity_dict Properly:
    Traceback (most recent call last):
      File "C:\Users\Waqas Hussain\OneDrive - Universit� degli Studi di Milano-Bicocca\Documents\GitHub\PZero\pzero\project_window.py", line 153, in <lambda>
        self.actionImportPyvista.triggered.connect(lambda: pyvista2vtk(self=self))
                                                           ^^^^^^^^^^^^^^^^^^^^^^
      File "C:\Users\Waqas Hussain\OneDrive - Universit� degli Studi di Milano-Bicocca\Documents\GitHub\PZero\pzero\imports\pyvista2vtk.py", line 83, in pyvista2vtk
        self.e_c.add_entity_from_dict(
        ^^^^^^^^
    AttributeError: 'ProjectWindow' object has no attribute 'e_c'
    
    Just like in dem2vtk.py, we create an entity_dict named curr_obj_attributes by deep copying self.geol_coll.entity_dict.
    We populate curr_obj_attributes with all necessary keys, including:
    uid: A unique identifier generated using str(uuid4()).
    name: The base name of the input file.
    topology: The type of the entity (e.g., "VertexSet", "PolyLine", etc.).
    vtk_obj: The curr_obj we read using pv_read.
    properties_names and properties_components: Empty lists, unless you have specific properties to add.
    Assigning the Correct Class to curr_obj:
    
    We set curr_obj.__class__ to the appropriate class from pzero.entities_factory based on the cell type.
    We also set a topology variable to use in the entity_dict.
    Adding the Entity to the Collection:
    
    We call self.geol_coll.add_entity_from_dict(entity_dict=curr_obj_attributes) to add the entity to the geological collection.
    Cleaning Up:
    
    We delete curr_obj and curr_obj_attributes to free up memory.

--- pzero/imports/pyvista2vtk.py
+++ pzero/imports/pyvista2vtk.py
@@ -1,16 +1,10 @@
-"""pyvista2vtk.py
-PZero© Andrea Bistacchi"""
-
 from uuid import uuid4
-
 from pyvista import read as pv_read
-
 from PyQt5.QtWidgets import QFileDialog
+from copy import deepcopy
+import os
 
 from pzero.entities_factory import VertexSet, PolyLine, TriSurf, TetraSolid
-
-"""TO BE COMPLETELY UPDATED - SEE DEM2VTK FOR INSTANCE _____________________________"""
-
 
 def pyvista2vtk(self):
     """
@@ -23,66 +17,58 @@ def pyvista2vtk(self):
         "Properties are discarded if they are not 1D, 2D, 3D, 4D, 6D or 9D (due to VTK limitations)"
     )
 
-    """Select and open input file"""
+    # Select and open input file
     in_file_name = QFileDialog.getOpenFileName(
         self, "Import entities from PyVista-supported file"
-    )
-    in_file_name = in_file_name[0]
+    )[0]
     if in_file_name:
         self.TextTerminal.appendPlainText("in_file_name: " + in_file_name)
-        """Initialize"""
+        # Initialize
         cell_type = -1
 
-        """Read file with pv_read() function and detect topology of curr_obj - ASSUMES ALL CELLS ARE OF THE SAME TYPE"""
+        # Read file with pv_read() function and detect topology
         try:
             curr_obj = pv_read(in_file_name)
 
-            """ VTK cell topology from documentation at https://vtk.org/doc/nightly/html/vtkCellType_8h.html
-            VTK_EMPTY_CELL = 0, VTK_VERTEX = 1, VTK_POLY_VERTEX = 2, VTK_LINE = 3,
-            VTK_POLY_LINE = 4, VTK_TRIANGLE = 5, VTK_TRIANGLE_STRIP = 6, VTK_POLYGON = 7,
-            VTK_PIXEL = 8, VTK_QUAD = 9, VTK_TETRA = 10, VTK_VOXEL = 11,
-            VTK_HEXAHEDRON = 12, VTK_WEDGE = 13, VTK_PYRAMID = 14, VTK_PENTAGONAL_PRISM = 15,
-            VTK_HEXAGONAL_PRISM = 16, VTK_QUADRATIC_EDGE = 21, VTK_QUADRATIC_TRIANGLE = 22, VTK_QUADRATIC_QUAD = 23,
-            VTK_QUADRATIC_POLYGON = 36, VTK_QUADRATIC_TETRA = 24, VTK_QUADRATIC_HEXAHEDRON = 25, VTK_QUADRATIC_WEDGE = 26,
-            VTK_QUADRATIC_PYRAMID = 27, VTK_BIQUADRATIC_QUAD = 28, VTK_TRIQUADRATIC_HEXAHEDRON = 29, VTK_QUADRATIC_LINEAR_QUAD = 30,
-            VTK_QUADRATIC_LINEAR_WEDGE = 31, VTK_BIQUADRATIC_QUADRATIC_WEDGE = 32, VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON = 33, VTK_BIQUADRATIC_TRIANGLE = 34,
-            VTK_CUBIC_LINE = 35, VTK_CONVEX_POINT_SET = 41, VTK_POLYHEDRON = 42, VTK_PARAMETRIC_CURVE = 51,
-            VTK_PARAMETRIC_SURFACE = 52, VTK_PARAMETRIC_TRI_SURFACE = 53, VTK_PARAMETRIC_QUAD_SURFACE = 54, VTK_PARAMETRIC_TETRA_REGION = 55,
-            VTK_PARAMETRIC_HEX_REGION = 56, VTK_HIGHER_ORDER_EDGE = 60, VTK_HIGHER_ORDER_TRIANGLE = 61, VTK_HIGHER_ORDER_QUAD = 62,
-            VTK_HIGHER_ORDER_POLYGON = 63, VTK_HIGHER_ORDER_TETRAHEDRON = 64, VTK_HIGHER_ORDER_WEDGE = 65, VTK_HIGHER_ORDER_PYRAMID = 66,
-            VTK_HIGHER_ORDER_HEXAHEDRON = 67, VTK_LAGRANGE_CURVE = 68, VTK_LAGRANGE_TRIANGLE = 69, VTK_LAGRANGE_QUADRILATERAL = 70,
-            VTK_LAGRANGE_TETRAHEDRON = 71, VTK_LAGRANGE_HEXAHEDRON = 72, VTK_LAGRANGE_WEDGE = 73, VTK_LAGRANGE_PYRAMID = 74,
-            VTK_BEZIER_CURVE = 75, VTK_BEZIER_TRIANGLE = 76, VTK_BEZIER_QUADRILATERAL = 77, VTK_BEZIER_TETRAHEDRON = 78,
-            VTK_BEZIER_HEXAHEDRON = 79, VTK_BEZIER_WEDGE = 80, VTK_BEZIER_PYRAMID = 81, VTK_NUMBER_OF_CELL_TYPES
-            """
-
-            """Get topology (CellType) of first cell in object - THEN ASSUMES ALL CELLS ARE OF THE SAME TYPE"""
+            # Get topology (CellType) of first cell in object
             cell_type = curr_obj.GetCellType(0)
-        except:
+        except Exception as e:
             self.TextTerminal.appendPlainText(
-                "pyvista2vtk - entity topology not recognized ERROR."
+                f"pyvista2vtk - entity topology not recognized ERROR: {e}"
             )
+            return  # Exit the function if reading fails
 
-        """If curr_obj is a recognized topology, assign to PZero class, and add to a collection"""
+        # If curr_obj is a recognized topology, assign to PZero class
         if cell_type == 1:
-            curr_obj.uid = str(uuid4())
-            curr_obj.type = "VertexSet"
             curr_obj.__class__ = VertexSet
+            topology = "VertexSet"
         elif cell_type == 3:
-            curr_obj.uid = str(uuid4())
-            curr_obj.type = "PolyLine"
             curr_obj.__class__ = PolyLine
+            topology = "PolyLine"
         elif cell_type == 5:
-            curr_obj.uid = str(uuid4())
-            curr_obj.type = "TriSurf"
             curr_obj.__class__ = TriSurf
+            topology = "TriSurf"
         elif cell_type == 10:
-            curr_obj.uid = str(uuid4())
-            curr_obj.type = "TetraSolid"
             curr_obj.__class__ = TetraSolid
-        self.e_c.add_entity_from_dict(
-            vtk_entity=curr_obj, entity_dict=deepcopy(GeologicalCollection().entity_dict)
-        )  # to APPROPRIATE collection_________________
+            topology = "TetraSolid"
+        else:
+            self.TextTerminal.appendPlainText(
+                "pyvista2vtk - unrecognized cell type."
+            )
+            return  # Exit if cell type is not recognized
 
-        """Clean"""
+        # Create the entity dictionary similar to dem2vtk.py
+        curr_obj_attributes = deepcopy(self.geol_coll.entity_dict)
+        curr_obj_attributes["uid"] = str(uuid4())
+        curr_obj_attributes["name"] = os.path.basename(in_file_name)
+        curr_obj_attributes["topology"] = topology
+        curr_obj_attributes["vtk_obj"] = curr_obj
+        curr_obj_attributes["properties_names"] = []  # Add if there are any properties
+        curr_obj_attributes["properties_components"] = []
+
+        # Add the entity to the geological collection
+        self.geol_coll.add_entity_from_dict(entity_dict=curr_obj_attributes)
+
+        # Clean up
         del curr_obj
+        del curr_obj_attributes
