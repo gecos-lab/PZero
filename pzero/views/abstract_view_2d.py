94fa8134fd2eae1128c00bf260724c966db93bd5
waqashussain117 <150252664+waqashussain117@users.noreply.github.com>
2025-09-05 07:28:47 +0000

    Apply prepare changes

--- pzero/views/abstract_view_2d.py
+++ pzero/views/abstract_view_2d.py
@@ -19,7 +19,6 @@ class View2D(ViewVTK):
         self.plotter.enable_image_style()
         self.plotter.enable_parallel_projection()
 
-
     # ================================  General methods shared by all views - built incrementally =====================
 
     def initialize_menu_tools(self):
