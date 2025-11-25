faa27daaceb9d6a688ce6428ab8b9f48ba200075
Riccardo Monti <r.monti14@campus.unimib.it>
2025-11-25 11:18:34 +0100

    editor_fixed

--- pzero/helpers/helper_widgets.py
+++ pzero/helpers/helper_widgets.py
@@ -156,7 +156,7 @@ class Editor(vtkContourWidget):
             self._event_translator.RemoveTranslation(vtkCommand.RightButtonPressEvent)
             self._event_translator.RemoveTranslation(vtkCommand.LeftButtonPressEvent)
             self.Render()
-            self.parent.plotter.track_click_position(
+            self._parent.plotter.track_click_position(
                 side="left", callback=self.select_point, viewport=True
             )
 
